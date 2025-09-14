cd739c41297e2707779f76c581ae1e1c795af710
John Mauricio <jxmauricio@Johns-MacBook-Pro.local>
2025-09-13 17:23:49 -0700

    scraped user question data

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,13 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.action === "getArticles") {
     try {
-        const articles = Array.from(document.querySelectorAll('article')[2].querySelector('.whitespace-pre-wrap').innerText);
-        sendResponse({articles : "this ia string"});
+        const articles = Array.from(document.querySelectorAll('article'))
+          .map(article => {
+            const el = article.querySelector('.whitespace-pre-wrap');
+            return el ? el.innerText : null;
+          }).filter(text => text !== null);
+        console.log(articles);
+        sendResponse({articles : articles});
     } catch (error) {
         sendResponse({ articles: "your query failed bro"});
     }
